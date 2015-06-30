66ecdb30d70e237e81390119fc4f881a84f454e9
Andrey <Andrey@test.com>
2015-06-30 19:51:53 +0300

    Xor

---
+++ src/DayThree/sumDigitsInLine.java
@@ -0,0 +1,18 @@
+package DayThree;
+import java.util.Scanner;
+
+public class sumDigitsInLine {
+    public static void main(String[] args) {
+        System.out.println("Число");
+        Scanner scn = new Scanner(System.in);
+        int n = 0;
+        n = scn.nextInt();
+        int x = n;
+        int s = 0;
+        while (x != 0) {
+            s += x % 10;
+            x /= 10;
+        }
+        System.out.println("Сума " + n + " = " + s);
+    }
+}

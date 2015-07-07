e37f3115465b4b290f25db85aab1c2c2edf8572d
Andrey <Andrey@test.com>
2015-07-07 21:42:02 +0300

    OOP 07.07.2015

---
+++ src/OOP1/StudentOOP2/API.java
@@ -0,0 +1,13 @@
+package OOP1.StudentOOP2;
+
+/**
+ * Created by ITHILLEL6 on 07.07.2015.
+ */
+public class API {
+    public static void main(String[] args) {
+        Person person = new Person();
+        Student student = new Student();
+        System.out.println(person.getId());
+        System.out.println(student.getId());
+    }
+}

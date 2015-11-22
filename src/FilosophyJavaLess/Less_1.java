85162391d170f8f8f93b04e0ee63a65c48b5c6ee
FaceOfGod <faceofgod22@gmail.com>
2015-11-23 01:28:34 +0200

    ll

---
+++ src/FilosophyJavaLess/Less_1.java
@@ -0,0 +1,15 @@
+package FilosophyJavaLess;
+
+/**
+ * Created by Сергей on 22.11.2015.
+ */
+
+public class Less_1 {
+    public static void main(String[] args) {
+        double distance = 155.5;
+        double time = 1.5;
+        double result = distance / time;
+        System.out.println("Speed = " + result);
+
+    }
+}
